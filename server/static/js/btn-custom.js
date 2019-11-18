7d41d76c404c95334740d3824ce42bdd8020bf0e
haihuam <haihuam@outlook.com>
2019-11-18 14:31:10 +0800

    support reschedule function

--- server/static/js/btn-custom.js
+++ server/static/js/btn-custom.js
@@ -1,12 +1,25 @@
 var deleteRun = function () {
-	var	run_id = $('#run_id').val();
-	console.log('Delete '+ run_id);
+	var	run_id = $('#delete_run_id').val();
+	console.log('log delete '+ run_id);
 	$.getJSON('/delete/'+run_id, function(data){
     console.log(data);
     location.reload();
   })
 };
 
-var values = function (ID) {
-		$('#run_id').val(ID);
+var rescheduleRun = function () {
+	var	run_id = $('#rescheduler_run_id').val();
+	console.log('log reschedule '+ run_id);
+	$.getJSON('/reschedule/'+run_id, function(data){
+    console.log(data);
+    location.reload();
+  })
+};
+
+var deleteValues = function (ID) {
+		$('#delete_run_id').val(ID);
+};
+
+var rescheduleValues = function (ID) {
+		$('#rescheduler_run_id').val(ID);
 };
